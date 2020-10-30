c6f59e5ac4300f6b147a8dd539baa3000156f79e
freakinyy <freakinyy@gmail.com>
2020-10-30 11:10:18 +0800

    Update shadowsocks-rust_server_installer_for_ubuntu.sh

--- shadowsocks-rust_server_installer_for_ubuntu.sh
+++ shadowsocks-rust_server_installer_for_ubuntu.sh
@@ -230,6 +230,7 @@ Add_to_Crontab(){
 	cat >> $TMP_DIR/crontab.bak <<EOF
 #shadowsocks-rust_server modifies start
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+30 16 * * 6 apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean
 00 17 * * 6 shadowsocks-rust_bin_installer.sh update
 #shadowsocks-rust_server modifies end
 EOF
